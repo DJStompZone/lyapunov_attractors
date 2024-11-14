73c8967d5a3dcc152812ce0c73d29a6c5a00ba11
SpectCOW <126259962+SpcFORK@users.noreply.github.com>
2024-11-14 15:42:25 -0500

    Update chaotic_system_finder.py to use colors and better display

--- lyapunov_attractors/chaotic_system_finder.py
+++ lyapunov_attractors/chaotic_system_finder.py
@@ -1,6 +1,9 @@
+import os
 from datetime import datetime
 from typing import List
 from pathlib import Path
+
+from colorama import Fore, Back, Style
 
 from lyapunov_attractors.attractor_system import AttractorSystem
 from lyapunov_attractors.storage_manager import StorageManager
@@ -8,6 +11,7 @@ from lyapunov_attractors.lyapunov_calculator import LyapunovCalculator
 from lyapunov_attractors.trajectory_simulator import TrajectorySimulator
 from lyapunov_attractors.visualizer import Visualizer
 from lyapunov_attractors.models import ChaoticSysFinderConfig, LyapConfig
+
 
 class ChaoticSystemFinder:
     """
@@ -31,6 +35,7 @@ class ChaoticSystemFinder:
         search(num_attempts: int = None):
             Searches for chaotic systems by simulating trajectories and evaluating their Lyapunov exponents.
     """
+
     def __init__(self, config: ChaoticSysFinderConfig | None = None):
         self.config = config or ChaoticSysFinderConfig()
         self.lyap_config: LyapConfig = self.config.lyap_config
@@ -38,7 +43,8 @@ class ChaoticSystemFinder:
         self.lyap_calculator = LyapunovCalculator(self.config.lyap_config)
         self.simulator = TrajectorySimulator(config, self.lyap_calculator)
         self.visualizer = Visualizer(Path("./best_attractors"))
-        self.best_systems: List[AttractorSystem] = self.storage_manager.load_systems()
+        self.best_systems: List[
+            AttractorSystem] = self.storage_manager.load_systems()
 
     def is_system_worthy(self, lyapunov: float) -> bool:
         """
@@ -56,7 +62,8 @@ class ChaoticSystemFinder:
             return True
         return lyapunov > min(system.lyapunov for system in self.best_systems)
 
-    def add_system(self, coefficients: List[float], lyapunov: float, points: List[List[float]]):
+    def add_system(self, coefficients: List[float], lyapunov: float,
+                   points: List[List[float]]):
         """
         Adds a new attractor system to the list of best systems if it meets the criteria.
 
@@ -78,8 +85,7 @@ class ChaoticSystemFinder:
             coefficients=coefficients,
             lyapunov=lyapunov,
             points=points,
-            timestamp=datetime.now().strftime("%Y%m%d_%H%M%S")
-        )
+            timestamp=datetime.now().strftime("%Y%m%d_%H%M%S"))
 
         self.best_systems.append(new_system)
         self.best_systems.sort(key=lambda x: x.lyapunov, reverse=True)
@@ -103,18 +109,36 @@ class ChaoticSystemFinder:
         if num_attempts is None:
             num_attempts = self.config.max_attempts
 
-        print(f"Searching for chaotic systems with Lyapunov exponent >= {self.lyap_config.lyapunov_threshold}")
+        print(
+            f"Searching for chaotic systems with Lyapunov exponent >= {self.lyap_config.lyapunov_threshold}"
+        )
         print(f"Currently stored systems: {len(self.best_systems)}")
 
         for attempt in range(num_attempts):
-            print(f"\rAttempt {attempt + 1}/{num_attempts}", end="")
+            print(f"\r{Fore.MAGENTA}{Back.YELLOW}" +
+                  f"Attempt {attempt + 1}/{num_attempts}" +
+                  f"{Fore.RESET}{Back.RESET}",
+                  end="")
 
             lyapunov, coefficients, points = self.simulator.simulate()
 
             if lyapunov > self.lyap_config.lyapunov_threshold:
-                print(f"\nFound system with Lyapunov exponent: {lyapunov:.3f}")
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print(
+                    f"{Fore.LIGHTYELLOW_EX}" +
+                    f"Found system with Lyapunov exponent: {lyapunov:.3f}" +
+                    f"{Fore.RESET} ({self.config.dimensions}D, {self.config.param_count} params)"
+                )
                 self.add_system(coefficients, lyapunov, points)
 
                 print("Current best systems:")
                 for idx, system in enumerate(self.best_systems, 1):
-                    print(f"{idx}. Lyapunov: {system.lyapunov:.3f} (found: {system.timestamp})")
+                    vxlen = min(len(system.points[0]), system.dimensions)
+                    print(
+                        f"{idx}. " +
+                        f"{Fore.LIGHTYELLOW_EX}Lyapunov: {system.lyapunov:.3f}{Fore.RESET} "
+                        +
+                        f"(found: {Fore.CYAN}{system.timestamp}{Fore.RESET})\n"
+                        +
+                        f'{Fore.RED}C   {system.coefficients[0:vxlen]}{Fore.RESET}\n'
+                        + f'{Fore.BLUE}Vx  {system.points[0]}{Fore.RESET}\n')
